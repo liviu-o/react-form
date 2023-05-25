e0860c5dbc3f5723afa84644f15284c54be25746
liviu-o <li.viu@mail.com>
2023-05-25 09:55:26 +0100

    implemented formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,103 @@
-import './App.css'
-import SignupForm from './components/SignupForm'
-import Header from './components/Header'
+import "./App.css";
+import SignupForm from "./components/SignupForm";
+import Header from "./components/Header";
 
 function App() {
- return (
+  const palaceList = [
+    {
+      name: "Hampton Court Palace",
+      value: "hamptonCourtPalace",
+    },
+    {
+      name: "Tower Of London",
+      value: "towerOfLondon",
+    },
+    {
+      name: "Banqueting House",
+      value: "banquetingHouse",
+    },
+    {
+      name: "Kensington Palace",
+      value: "kensingtonPalace",
+    },
+    {
+      name: "Kew Palace",
+      value: "kewPalace",
+    },
+    {
+      name: "Hillsborough Castle",
+      value: "hillsboroughCastle",
+    },
+  ];
+
+  const countryList = [
+    {
+      name: "United Kingdom",
+      value: "GB",
+    },
+    {
+      name: "Australia",
+      value: "AU",
+    },
+    {
+      name: "Brazil",
+      value: "BR",
+    },
+    {
+      name: "Canada",
+      value: "CA",
+    },
+    {
+      name: "China",
+      value: "CN",
+    },
+    {
+      name: "France",
+      value: "FR",
+    },
+    {
+      name: "Germany",
+      value: "DE",
+    },
+    {
+      name: "Ireland",
+      value: "IE",
+    },
+    {
+      name: "Italy",
+      value: "IT",
+    },
+    {
+      name: "Japan",
+      value: "JP",
+    },
+    {
+      name: "Portugal",
+      value: "PT",
+    },
+    {
+      name: "Russia",
+      value: "RU",
+    },
+    {
+      name: "Spain",
+      value: "ES",
+    },
+    {
+      name: "United States",
+      value: "US",
+    },
+    {
+      name: "Other",
+      value: "other",
+    },
+  ];
+  return (
     <div>
       <Header />
-      <SignupForm />
+      <SignupForm palaces={palaceList} countries={countryList} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
